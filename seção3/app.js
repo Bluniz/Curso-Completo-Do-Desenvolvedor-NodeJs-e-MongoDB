f604194d77d03c98d0bc28f2f3d63c56af10c3a8
Lucas Rosa <lucassantosrosa51@gmail.com>
2020-12-15 17:34:07 -0300

    Adiciona Seção 3

---
+++ seção3/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+//? const express = require('express')();
+/*
+! O Require do express retorna uma função que precisa ser executada
+ */
+const app = express();
+//! Altera a engine de view do Express e define o modulo que irá substituir!
+app.set('view engine', 'ejs')
+
+//* Este metodo fica escutando requisições em uma determinada porta
+app.listen(3000, function () {
+  console.log("funfou");
+});
+
+app.get("/", function (req, res) {
+  res.render("home/index");
+});
+
+app.get("/formulario_inclusao_noticia", function (req, res) {
+  res.render("admin/form_add_noticia");
+});
+
+app.get("/noticias", function (req, res) {
+  res.render("noticias/noticias");
+});
+
+/* 
+  app.get("/tecnologia", function (req, res) {
+  ! Graças ao ejs a responder agora tem a propriedade render!
+  ! Passa como parametro o arquivo de view ejs que deseja renderizar!
+  ! Como a engine já sabe que esta trabalhando com ejs, não precisa colocar a extensão .ejs
+  res.render("secao/tecnologia");
+});
+ */
+
+ //! Nodemon serve para identificar as alterações e reiniciar o servidor
+
