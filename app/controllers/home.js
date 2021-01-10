7a2238ed0cedbda24768fb9de75e8ae1090a6354
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-01-10 19:56:07 -0300

    Organizing the folders of projects

--- app/controllers/home.js
+++
@@ -1,8 +0,0 @@
-module.exports.index = function (app, req, res) {
-  const connection = app.config.dbConnection();
-  const noticiasModel = new app.app.models.noticiasDAO(connection);
-
-  noticiasModel.getLastFiveNoticies(function (error, result) {
-    res.render("home/index", { notices: result });
-  });
-};
