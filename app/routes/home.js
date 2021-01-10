314debeb0fccf07ba823abcc708e2ceccdeb83f9
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-01-09 21:55:49 -0300

    Add section 11

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,6 +1,6 @@
 //! Quando ela for chamada, irá executar a função no seu escopo.
 module.exports = function (app) {
   app.get("/", function (req, res) {
-    res.render("home/index");
+    app.app.controllers.home.index(app, req, res);
   });
 };
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,23 +1,10 @@
 //const dbConnection = require("../../config/dbConnection");
-
 module.exports = function (app) {
   app.get("/noticias", function (req, res) {
-    const dbConnection = app.config.dbConnection;
+    app.app.controllers.noticias.noticias(app, req, res);
+  });
 
-    const connection = dbConnection();
-    const noticiasModel = new app.app.models.noticiasDAO(connection);
-
-    noticiasModel.getNoticias( function (error, result) {
-      //! O EJS permite enviar dados como segundo parametro e na view identificar os dados.
-     
-      res.render("noticias/noticias", { noticias: result });
-    });
-
-    //! Feita a conexão, podemos realizar consultas/comandos SQL com connection.query()
-    //? A função query espera duas coisas, o SQL e uma função de callback: .query(<SQL>, <Função de Callback>)
-    /* d, function (error, result) {
-      //! O EJS permite enviar dados como segundo parametro e na view identificar os dados.
-      res.render("noticias/noticias", { noticias: result });
-    }); */
+  app.get("/noticia", function (req, res) {
+    app.app.controllers.noticias.noticia(app, req, res);
   });
 };
